33c27115ea7f49370e0a58a168e7802cf739958f
aarianna <ascott@wlapcs.org>
2017-03-20 20:19:22 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup(){
+    createCanvas(600,400);
+    
+}
+
+function draw(){
+    line(35,80,400,200);
+    line(66,80,480,66);
+    line(99,99,99,99);
+    line(127,64,122,222);
+    line(555,42,112,113);
+
+    stroke(255,225,255);
+    fill(255,0,0);
+    rect(290,100,250,250);
+
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(0,0,255);
+    ellipse(50,250,80,80);
+    fill(244, 66, 226);
+    ellipse(50,75,90,90);
+    
+    
+}
+
+// draw me 2 ellipse
+// draw me 2 rectangles
+// draw em 4 lines (1 with a postive slop, 1 with a neg slope,)
+// 1 with a slope of 0, and 1 with a slop with a slope of underfined.
